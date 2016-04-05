5f50bf8655773f89ae8cf945e3c37f7e7486f6d6
Connor Dodge <Connor Dodge>
2016-04-05 12:26:10 -0600

    add method working, still need to fix form submission

--- src/js/controllers.js
+++ src/js/controllers.js
@@ -1,7 +1,24 @@
 (function(){
 	angular.module('todoDisplay',[]).
 		controller('TodoController',['Item', function(Item){
-			console.log("con");
-			alert(Item.test());
+			var vm = this
+			Item.list().then(function(response){
+				vm.list = response.data;
+				console.log(vm.list);
+			});
+			vm.newTodo = {
+				$$hashkey:null,
+				description:null,
+				done: 0,
+				id: null,
+				title:null
+			}
+			if(vm.newTodo.description){
+				Item.add(vm.newTodo.title, vm.newTodo.description).then(function(response){
+					vm.newTodo.id = response.id;
+					vm.list.push(vm.newTodo);
+					console.log(vm.list);
+				});
+			}
 		}]);
 })();
--- src/js/services.js
+++ src/js/services.js
@@ -1,15 +1,25 @@
 (function(){
 	angular.module('todoDisplay')
-		.factory('Item', item);
+		.factory('Item',['$http', item]);
 
-		function item(){
+		function item($http){
 			service = {
-				test: test
+				test: test,
+				list: list,
+				 add: add,
 			}
 			return service
 
 			function test(){
 				return 'Hello World';
+			}
+
+			function list(){
+				return $http.get('http://secret-escarpment-99471.herokuapp.com/item');
+			}
+
+			function add(title,description){
+				return $http.post('http://secret-escarpment-99471.herokuapp.com/item', {title:title, description:description});
 			}
 		}
 })();
